339d79df805ebe694a16b5b2825ac1574179ea71
Brian Tham <briantham1@gmail.com>
2024-02-19 21:36:27 +0800

    Implement dispose in GameObjectManager

--- core/src/com/bullethell/game/gameObject/GameObjectManager.java
+++ core/src/com/bullethell/game/gameObject/GameObjectManager.java
@@ -2,34 +2,46 @@ package com.bullethell.game.gameObject;
 
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.utils.Array;
+import com.badlogic.gdx.utils.Disposable;
 
-public class GameObjectManager {
+public class GameObjectManager implements Disposable {
+    private Array<GameObject> gameObjects;
 
-	private Array<GameObject> gameObjects;
-	
-	public GameObjectManager()
-	{
-		gameObjects = new Array<>();
-	}
-	
+    public GameObjectManager()
+    {
+        gameObjects = new Array<>();
+    }
+
     public void addGameObject(GameObject gameObject)
     {
         gameObjects.add(gameObject);
     }
-    
+
     public void update(float delta)
     {
-        for (GameObject obj : gameObjects)
+        for (GameObject gameObject : gameObjects)
         {
-            obj.update(delta);
+            gameObject.update(delta);
         }
     }
-    
+
     public void draw(SpriteBatch batch)
     {
-    	for (GameObject obj : gameObjects)
-    	{
-    		obj.draw(batch);
-    	}
+        for (GameObject gameObject : gameObjects)
+        {
+            gameObject.draw(batch);
+        }
+    }
+
+    @Override
+    public void dispose()
+    {
+        for (GameObject gameObject : gameObjects)
+        {
+            if (gameObject instanceof Disposable)
+            {
+                ((Disposable) gameObject).dispose();
+            }
+        }
     }
 }
