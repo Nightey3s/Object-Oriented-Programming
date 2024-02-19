92c8ebad89d33ab2f270fc763d6b7556f3b2319b
Brian Tham <briantham1@gmail.com>
2024-02-19 17:34:52 +0800

    Fix code indentation

--- core/src/com/bullethell/game/gameObject/PowerUp.java
+++ core/src/com/bullethell/game/gameObject/PowerUp.java
@@ -3,18 +3,22 @@ package com.bullethell.game.gameObject;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.math.Vector2;
 
-public class PowerUp extends GameObject {
-    public PowerUp(Vector2 position, Vector2 size) {
+public class PowerUp extends GameObject{
+	
+    public PowerUp(Vector2 position, Vector2 size)
+    {
         super(position, size);
     }
 
     @Override
-    public void update(float delta) {
+    public void update(float delta)
+    {
 
     }
 
     @Override
-    public void draw(SpriteBatch batch) {
+    public void draw(SpriteBatch batch)
+    {
 
     }
 }
