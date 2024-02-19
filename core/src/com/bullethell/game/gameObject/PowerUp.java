f1e7bbb22b3df945b2d8b52a14743310cbffa6a5
Brian Tham <briantham1@gmail.com>
2024-02-19 17:06:22 +0800

    Create PowerUp class

---
+++ core/src/com/bullethell/game/gameObject/PowerUp.java
@@ -0,0 +1,20 @@
+package com.bullethell.game.gameObject;
+
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.Vector2;
+
+public class PowerUp extends GameObject {
+    public PowerUp(Vector2 position, Vector2 size) {
+        super(position, size);
+    }
+
+    @Override
+    public void update(float delta) {
+
+    }
+
+    @Override
+    public void draw(SpriteBatch batch) {
+
+    }
+}
