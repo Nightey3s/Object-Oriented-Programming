d79be0068b7b89213027d449c666cb901a890e0e
Brian Tham <briantham1@gmail.com>
2024-02-19 16:19:33 +0800

    Created Enemy class

---
+++ core/src/com/bullethell/game/gameObject/Enemy.java
@@ -0,0 +1,30 @@
+package com.bullethell.game.gameObject;
+
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.Vector2;
+
+public class Enemy extends GameObject implements IMovable {
+	
+	public Enemy(Vector2 position, Vector2 size)
+	{
+		super(position, size);
+	}
+	
+	@Override
+	public void move(float delta)
+	{
+		
+	}
+	
+	@Override
+	public void update(float delta)
+	{
+		move(delta);
+	}
+	
+	@Override
+	public void draw(SpriteBatch batch)
+	{
+		
+	}
+}
