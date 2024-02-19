d7e3328a88d5e729dba64e054a74a43d470def20
Brian Tham <briantham1@gmail.com>
2024-02-19 21:37:00 +0800

    Modify GameObject children

--- core/src/com/bullethell/game/gameObject/PowerUp.java
+++ core/src/com/bullethell/game/gameObject/PowerUp.java
@@ -1,24 +1,32 @@
 package com.bullethell.game.gameObject;
 
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.graphics.Texture;
 
-public class PowerUp extends GameObject{
-	
-    public PowerUp(Vector2 position, Vector2 size)
+public class PowerUp extends GameObject {
+    private Texture texture;
+
+    public PowerUp(float x, float y)
     {
-        super(position, size);
+        super(x, y, 48, 48); // Example size, adjust as needed
+        texture = new Texture("TODO");
     }
 
     @Override
     public void update(float delta)
     {
-
+        // PowerUp specific logic here
     }
 
     @Override
     public void draw(SpriteBatch batch)
     {
+        batch.draw(texture, bounds.x, bounds.y);
+    }
 
+    @Override
+    public void dispose()
+    {
+        texture.dispose();
     }
 }
--- core/src/com/bullethell/game/gameObject/Projectile.java
+++ core/src/com/bullethell/game/gameObject/Projectile.java
@@ -1,30 +1,38 @@
 package com.bullethell.game.gameObject;
 
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.graphics.Texture;
 
 public class Projectile extends GameObject implements IMovable {
+    private Texture texture;
 
-		public Projectile(Vector2 position, Vector2 size)
-		{
-			super(position, size);
-		}
-		
-		@Override
-		public void move(float delta)
-		{
-			
-		}
-		
-		@Override
-		public void update(float delta)
-		{
-			move(delta);
-		}
-		
-		@Override
-		public void draw(SpriteBatch batch)
-		{
-			
-		}
+    public Projectile(float x, float y)
+    {
+        super(x, y, 32, 32); // Example size, adjust as needed
+        texture = new Texture("TODO");
+    }
+
+    @Override
+    public void move(float delta)
+    {
+        // Implement projectile movement logic here
+    }
+
+    @Override
+    public void update(float delta)
+    {
+        move(delta);
+    }
+
+    @Override
+    public void draw(SpriteBatch batch)
+    {
+        batch.draw(texture, bounds.x, bounds.y);
+    }
+
+    @Override
+    public void dispose()
+    {
+        texture.dispose();
+    }
 }
