941d7821266d2fc16758fd66ffe7a97d83e82546
HaaL01 <50935798+HaaL01@users.noreply.github.com>
2024-02-20 16:03:18 +0800

    feat: Implemented collision detection

--- core/src/com/bullethell/game/collision/CollisionManager.java
+++ core/src/com/bullethell/game/collision/CollisionManager.java
@@ -2,6 +2,8 @@ package com.bullethell.game.collision;
 
 import com.badlogic.gdx.utils.Array;
 import com.bullethell.game.gameObject.GameObject;
+import com.bullethell.game.gameObject.Enemy;
+import com.bullethell.game.gameObject.Player;
 
 public class CollisionManager implements iCollision{
     private Array<GameObject> collisionList;
@@ -11,15 +13,59 @@ public class CollisionManager implements iCollision{
         this.collisionList = new Array<GameObject>();
     }
 
+    // getters and setters
+    public Array<GameObject> getCollisionList() {
+        return collisionList;
+    }
+
+    public GameObject getCollisionItem(int index) {
+        return collisionList.get(index);
+    }
+
     @Override
     public void isCollidable(GameObject Object) { // Interface connects to gameObject and appends to CollisionList
         collisionList.add(Object);
     }
 
+    public void removeCollidable(GameObject Object) {
+        collisionList.removeValue(Object, true);
+    }
+
     // Might have to add remove method depending on implementation of destroying objects.
 
-    public void checkCollision(GameObject Object1, GameObject Object2) {
-
+    public boolean checkCollision(GameObject Object1, GameObject Object2) {
+        return Object1.getBounds().overlaps(Object2.getBounds());
     }
+
+    // 1. Iterate through the collisionList > 2. Check for collision (obj1,obj2) > 3. Resolve collision
+    
+
+
+    public void resolveCollision(GameObject Object1, GameObject Object2) {  
+        if (Object1 instanceof Player && Object2 instanceof Enemy) {
+            // // if object 1 is a player and object 2 is an enemy, object 1 takes damage and object 2 is destroyed.
+        }
+        else if (Object1 instanceof Enemy && Object2 instanceof Player) {
+            // Then do something
+        }
+        else if (Object1 instanceof Enemy && Object2 instanceof Enemy) {
+            // Then do something
+        }
+        else if (Object1 instanceof Player && Object2 instanceof Player) {
+            // Then do something
+        }
+    }
+
+    public void getBounds(Enemy triangle) {
+        // EMPTY
+    };
+
+    public void getBounds(Player rectangle) {
+        // EMPTY
+    };
+
+    // Implement listener function to check for collision
+
+    
 
 }
