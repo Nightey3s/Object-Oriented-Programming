e4bb9198fb6c1fe8300838e330f5cb9c2f4e9efb
Brian Tham <briantham1@gmail.com>
2024-02-21 20:39:48 +0800

    fix

--- core/src/com/bullethell/game/MainGameClass.java
+++ core/src/com/bullethell/game/MainGameClass.java
@@ -1,26 +1,23 @@
 package com.bullethell.game;
 
-import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
 import com.bullethell.game.IO.InputManager;
 import com.bullethell.game.gameObject.GameObjectManager;
 import com.bullethell.game.scene.SceneManager;
 
-public class MainGameClass extends ApplicationAdapter {
+public class MainGameClass {
     GameObjectManager gameObjectManager;
     ShapeRenderer shapeRenderer;
     SceneManager sceneManager;
     InputManager inputManager;
     
-    @Override
-    public void create () {
+    public MainGameClass () {
         gameObjectManager = new GameObjectManager();
         shapeRenderer = new ShapeRenderer();
         sceneManager = new SceneManager();
         inputManager = new InputManager();
     }
     
-    @Override
     public void render() {
         
         InputManager.ScreenControl(sceneManager);
@@ -28,7 +25,6 @@ public class MainGameClass extends ApplicationAdapter {
   
     }
     
-    @Override
     public void dispose () {
         gameObjectManager.dispose();
         shapeRenderer.dispose(); // Dispose of the ShapeRenderer
