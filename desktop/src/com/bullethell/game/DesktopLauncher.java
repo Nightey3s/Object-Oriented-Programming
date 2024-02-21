1cef9381e4dd610978e5ed4ee6251238d1163279
Brian Tham <briantham1@gmail.com>
2024-02-21 20:39:56 +0800

    point desktoplauncher to main

--- desktop/src/com/bullethell/game/DesktopLauncher.java
+++ desktop/src/com/bullethell/game/DesktopLauncher.java
@@ -10,6 +10,6 @@ public class DesktopLauncher {
 		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
 		config.setForegroundFPS(60);
 		config.setTitle("bullet hell");
-		new Lwjgl3Application(new MainGameClass(), config);
+		new Lwjgl3Application(new Main(), config);
 	}
 }
