b83cbdcb390db7f5e035f28140659c79cea82a32
Brian Tham <briantham1@gmail.com>
2024-03-28 16:57:43 +0800

    Make BigRubbish spawn higher, to avoid collision and getting destroyed on instantiation

--- core/src/com/bullethell/game/gameObject/GameObjectManager.java
+++ core/src/com/bullethell/game/gameObject/GameObjectManager.java
@@ -30,7 +30,7 @@ public class GameObjectManager implements Disposable {
 		aiManager = new AiManager();
 		// createPlayer(100, 100);
 		createEnemy(200, 800);
-		createBigRubbish(300, 700, 100, 100);    
+		createBigRubbish(300, 900, 100, 100);    
 		createPowerUp(400, 400);
 		createEarth(0, -250, Gdx.graphics.getWidth(), Gdx.graphics.getWidth());
 		createShip(200, 200, this, this.sceneManager);
