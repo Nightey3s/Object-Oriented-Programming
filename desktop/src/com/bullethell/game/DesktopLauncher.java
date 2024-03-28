f1780a737172554812a4a3255906f1d016bcd9b0
Sherwyn-Chan <sherwyn555@gmail.com>
2024-03-28 18:56:43 +0800

    deleted useless class objects. Set resizing of the screen to false.

--- desktop/src/com/bullethell/game/DesktopLauncher.java
+++ desktop/src/com/bullethell/game/DesktopLauncher.java
@@ -1,15 +1,17 @@
 package com.bullethell.game;
 
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
 import com.bullethell.game.MainGameClass;
 
 // Please note that on macOS your application needs to be started with the -XstartOnFirstThread JVM argument
 public class DesktopLauncher {
-	public static void main (String[] arg) {
+	public static void main(String[] arg) {
 		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
-		config.setForegroundFPS(60);
 		config.setTitle("bullet hell");
+		config.setResizable(false);
+		config.setForegroundFPS(60);
 		new Lwjgl3Application(new Main(), config);
 	}
 }
