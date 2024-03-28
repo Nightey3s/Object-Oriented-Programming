438ddf97e3004fd56033040807dd1a84bcc51505
lleslie137 <94502770+lleslie137@users.noreply.github.com>
2024-03-28 19:26:52 +0800

    Refactored ObjectFactory

--- core/src/com/bullethell/game/Factory/ObjectFactory.java
+++ core/src/com/bullethell/game/Factory/ObjectFactory.java
@@ -14,6 +14,11 @@ public class ObjectFactory {
 	private static final String BR_ASSET = "BigRubbish.png";
 	private static final float SR_SPEED = 100;
 	private static final float BR_SPEED = 150;
+	private static final int BR_W= 100;
+	private static final int BR_H= 100;
+	private static final int SR_W = 40;
+	private static final int SR_H = 50;
+	
 	private static final List<String> SMALLTEXTURE_PATHS = new ArrayList<String>() {{
 		add("battery.png");
 		add("styrofoam_cup.png");
@@ -48,12 +53,12 @@ public class ObjectFactory {
 		return new Ship(x, y, gameObjectManager, sceneManager);
 	}
 
-	public static GameObject createBigRubbish(float x, float y, int width, int height) {
-		return new BigRubbish(x, y, width, height, BR_ASSET);
+	public static GameObject createBigRubbish(float x, float y) {
+		return new BigRubbish(x, y, BR_W, BR_H, BR_ASSET);
 	}
 
-	public static GameObject createSmallRubbish(float x, float y, int width, int height, String texfilepath) {
-		return new SmallRubbish(x, y, width, height, texfilepath);
+	public static GameObject createSmallRubbish(float x, float y, String texfilepath) {
+		return new SmallRubbish(x, y, SR_W, SR_H, texfilepath);
 	}
 
 	public static Enemy createEnemy(float x, float y) {
